193b8b39c5eacf8eadd4221befd9a84cb507062c
Userhl0133 <167547493+Userhl0133@users.noreply.github.com>
2024-10-31 02:11:29 +0800

    Update Pharamacist.java

--- Hospital_Management_System/src/main/java/HMS/models/Pharamacist.java
+++ Hospital_Management_System/src/main/java/HMS/models/Pharamacist.java
@@ -1,5 +1,7 @@
 package HMS.models;
 
-public class Pharamacist {
+public class Pharamacist extends User {
+
+    public void viewAppointmentOutcomeRecord() { // Implementation code here System.out.println("Viewing appointment outcome record for pharmacist " + name + "."); } public void updatePrescriptionStatus(int prescriptionId, String status) { // Implementation code here System.out.println("Updated prescription " + prescriptionId + " to status '" + status + "'."); } public void viewInventory() { // Implementation code here System.out.println("Displaying current inventory."); } public ReplenishmentRequest submitReplenishmentRequest(String medicationName, int stockLevel) { // Create a new replenishment request System.out.println("Submitting replenishment request for " + medicationName + " with stock level " + stockLevel + "."); return new ReplenishmentRequest(this.id, 0, medicationName, stockLevel);
     
 }

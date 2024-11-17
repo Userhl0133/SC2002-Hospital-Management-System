a5a2b862580293afa855064333abf479f25e13a8
Nga Wei En <53991158+weien26@users.noreply.github.com>
2024-11-18 00:25:34 +0800

    Comments

--- Hospital_Management_System/src/main/java/HMS/service/IFileService.java
+++ Hospital_Management_System/src/main/java/HMS/service/IFileService.java
@@ -4,13 +4,22 @@ import java.util.List;
 import HMS.models.Patient;
 import HMS.models.Medication;
 
+// Interface for FileService
 public interface IFileService {
+    // method to get patients data
     List<Patient> getPatientsData(String filePath);
+    // method to get staff data
     List<Object> getStaffData(String filePath);
+    // method to get medications data
     List<Medication> getMedicationsData(String filePath);
+    // method to write patients data
     void writePatientsData(String filePath);
+    // method to write staff data
     void writeStaffData(String filePath);
+    // method to write medications data
     void writeMedicationData(String filePath);
+    // method to get doctor availability
     void getDoctorAvailability(String filePath);
+    // method to write doctor availability
     void writeDoctorAvailability(String filePath);
 }
